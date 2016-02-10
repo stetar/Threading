261983ac919665f82fbc906a1682f800d25983b8
NearlyBatman <denskyldige@hotmail.com>
2016-02-10 10:29:46 +0100

    Dis inn

---
+++ Projekt/Projekt/Inn.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    class Inn : GameObject
+    {
+        public Inn(string imagepath, Vector2D startpos, float scalefactor) : base(imagepath, startpos, scalefactor)
+        {
+
+        }
+
+    }
+}
