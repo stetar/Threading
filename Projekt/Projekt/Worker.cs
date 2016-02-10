280ad7eaafd0b12096b1bf769869b777b823bce0
NearlyBatman <denskyldige@hotmail.com>
2016-02-10 09:49:42 +0100

    All dis

--- Projekt/Projekt/Worker.cs
+++ Projekt/Projekt/Worker.cs
@@ -8,5 +8,43 @@ namespace Projekt
 {
     class Worker : GameObject
     {
+        float speed;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="imagepath"></param>
+        /// <param name="startPos"></param>
+        /// <param name="scalefactor"></param>
+        public Worker(float speed, string imagepath, Vector2D startPos, float scalefactor) : base(imagepath, startPos, scalefactor)
+        {
+            this.speed = speed;
+        }
+        public override void Update(float fps)
+        {
+            if (gold < 5)
+            {
+                Vector2D velocity = this.position.Subtract(inn.position);
+            }
+           if (gold > 5)
+            {
+                Vector2D velocity = this.position.Subtract(farm.position);
+            }
+        }
+        public override void OnCollision(GameObject other)
+        {
+            if (other is Inn)
+            {
+                if (gold >= 5)
+                {
+                    Death += 1;
+                }
+                
+            }
+
+            if (other is Farm)
+            {
+
+            }
+        }
     }
 }
